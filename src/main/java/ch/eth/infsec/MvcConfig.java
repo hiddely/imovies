9552a634e1aba76858c042cc20ff6d4e3572acc9
Hidde Lycklama <hidde@vlinderstorm.com>
2016-10-05 18:48:19 +0200

    Add start of templating Add security config Add mvc config Add bootstrap

---
+++ src/main/java/ch/eth/infsec/MvcConfig.java
@@ -0,0 +1,13 @@
+package ch.eth.infsec;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+
+
+}
---
+++ src/main/java/ch/eth/infsec/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package ch.eth.infsec;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/css/**").permitAll().anyRequest().permitAll();
+        http.authorizeRequests().antMatchers("/js/**").permitAll().anyRequest().permitAll();
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+
+
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        /*auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");*/
+
+        auth
+                .jdbcAuthentication()
+                .authoritiesByUsernameQuery(getUserQuery());
+    }
+
+    private String getUserQuery() {
+        return "SELECT email as username, pwd as password "
+                + "FROM users"
+                + "WHERE email = ?";
+    }
+}
