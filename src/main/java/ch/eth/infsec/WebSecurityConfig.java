785d7b02bbf6ecb00fef849b58675f83c89eaf41
FR4NK-W <wirzf@student.ethz.ch>
2016-10-12 21:13:44 +0000

    Enable proper logout
    Redirect user to login page after logout and don't grant site access anymore

--- src/main/java/ch/eth/infsec/WebSecurityConfig.java
+++ src/main/java/ch/eth/infsec/WebSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -32,8 +33,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .defaultSuccessUrl("/account", true)
                 .permitAll()
                 .and()
-                .logout()
-                .permitAll();
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login");
 
         // Configure X509 authentication
         // CN holds the username (in our case, email address)
