95e9589d18f7fec2ee10134e7ee38fbbe2ea00a3
Fabian <fmurer@student.ethz.ch>
2016-10-27 13:02:25 +0200

    modified

--- virtual-machines/hardening.sh
+++ virtual-machines/hardening.sh
@@ -15,11 +15,10 @@ iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 
 
 ### HTTP / HTTPS ###
-iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 
-iptables -A INPUT -p udp --dport 80 -j ACCEPT
-iptables -A INPUT -p udp --dport 443 -j ACCEPT
+iptables -A INPUT -i eth0 -p udp -m multiport --sport 53,67 -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -i eth0 -p tcp -m multiport --sport 53,80,443,8080 -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -i eth0 -p udp -m multiport --sport 53,80,443,8080 -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 
 exit
