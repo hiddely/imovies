aeabec8bd21e68db149cd12e94613c2c6f6990ed
FR4NK-W <wirzf@student.ethz.ch>
2016-11-12 14:32:09 +0100

    SSH Tunnel the database connection
    SSH Tunnel localhost:3306 webservice to localhost:3306 dbserver to have encrypted and authenticated webservice <-> dbserver communication

--- virtual-machines/dbserver/setup_db.sh
+++ virtual-machines/dbserver/setup_db.sh
@@ -6,5 +6,3 @@ mysql -uroot -proot imovies < /home/vagrant/imovies_users.dump
 mysql -uroot -proot -e "CREATE USER 'webservice'@'%' IDENTIFIED BY 'webservice';"
 mysql -uroot -proot -e "GRANT USAGE ON *.* TO 'webservice'@'%';"
 mysql -uroot -proot -e "GRANT SELECT ON imovies.* TO 'webservice'@'%';"
-# comment out bind address in /etc/mysql/mysql.conf.d/mysqld.cnf
-# and restart mysql: sudo service mysql restart
